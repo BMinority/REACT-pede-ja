bd3198a9bb2b7dd9ff3d2a4fce81a1378f4a925d
Bruno Coelho <brunocoelho66@gmail.com>
2024-09-26 12:16:08 -0300

    feat navigation responsive

--- pede-ja/src/components/jsx/Navigation.jsx
+++ pede-ja/src/components/jsx/Navigation.jsx
@@ -1,36 +1,65 @@
-import '../css/Navigation.css'
+import { useState, useEffect } from 'react';
+import '../css/Navigation.css';
 
 function Navigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [activeItem, setActiveItem] = useState('#home');
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleItemClick = (hash) => {
+        setActiveItem(hash);
+        setIsMenuOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isMenuOpen && !event.target.closest('.navigation')) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isMenuOpen]);
+
     return (
-        <div className='navigation'>
+        <div className={`navigation ${isMenuOpen ? 'open' : ''}`}>
             <section className='box-nav'>
                 <nav>
                     <ul>
-                        <li>
-                            <a href="#home">
+                        <li className={activeItem === '#home' ? 'active' : ''}>
+                            <a href="#home" onClick={() => handleItemClick('#home')}>
                                 Home
                             </a>
                         </li>
-                        <li>
-                            <a href="#promo">
+                        <li className={activeItem === '#promo' ? 'active' : ''}>
+                            <a href="#promo" onClick={() => handleItemClick('#promo')}>
                                 Promoções
                             </a>
                         </li>
-                        <li>
-                            <a href="#pedeLover">
+                        <li className={activeItem === '#pedeLover' ? 'active' : ''}>
+                            <a href="#pedeLover" onClick={() => handleItemClick('#pedeLover')}>
                                 Pede Lover
                             </a>
                         </li>
-                        <li>
-                            <a href="#pedeCard">
+                        <li className={activeItem === '#pedeCard' ? 'active' : ''}>
+                            <a href="#pedeCard" onClick={() => handleItemClick('#pedeCard')}>
                                 Pede Card
                             </a>
                         </li>
                     </ul>
                 </nav>
             </section>
+            <div className='navigation-btn' onClick={toggleMenu}>
+                <i className={`fa-solid ${isMenuOpen ? 'fa-angle-left' : 'fa-angle-right'}`}></i>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navigation
+export default Navigation;
