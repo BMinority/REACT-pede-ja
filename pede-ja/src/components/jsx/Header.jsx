d51bc7edde333e2d4790d49093c07b093406e627
Bruno Coelho <brunocoelho66@gmail.com>
2024-09-27 15:09:10 -0300

    feat dropdown-menu header

--- pede-ja/src/components/jsx/Header.jsx
+++ pede-ja/src/components/jsx/Header.jsx
@@ -5,9 +5,14 @@ import '../css/Header.css';
 
 function Header() {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleLoginModal = () => {
         setIsLoginModalOpen(!isLoginModalOpen);
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
@@ -15,11 +20,27 @@ function Header() {
             <div className="pede-icon">
                 <img src={PedeIcon} alt="Ícone do Pede Já" />
             </div>
+
+            <div className="menu-hamburguer" onClick={toggleMenu}>
+                <i className={`fa-solid ${isMenuOpen ? 'fa-xmark' : 'fa-bars'}`}></i>
+            </div>
+
+            {isMenuOpen && (
+                <div className="dropdown-menu">
+                    <Link to="/sobre-nos" onClick={toggleMenu}>Sobre nós</Link>
+                    <Link to="/vagas" onClick={toggleMenu}>Vagas</Link>
+                    <Link to="/contato" onClick={toggleMenu}>Contato</Link>
+                    <Link to="/create-account" onClick={toggleMenu}>Criar conta</Link>
+                    <button className="enter" onClick={toggleLoginModal}>ENTRAR</button>
+                </div>
+            )}
+
             <div className="pede-options">
                 <Link to="/sobre-nos">Sobre nós</Link>
                 <Link to="/vagas">Vagas</Link>
                 <Link to="/contato">Contato</Link>
             </div>
+
             <div className="pede-login">
                 <Link to="/create-account">Criar conta</Link>
                 <button className="enter" onClick={toggleLoginModal}>ENTRAR</button>
